2986631e80f7a160876feed175f522c2644dc336
Niek Esselink <mail@niekers.nl>
2011-03-05 02:07:54 +0100

    Adding new files

---
+++ Install/install_main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Corire.Install
+{
+    public partial class install_main : Form
+    {
+        public install_main()
+        {
+            InitializeComponent();
+        }
+    }
+}
